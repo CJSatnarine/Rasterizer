143fe89f84590ff9770e6266e6411883367e3ba6
CJSatnarine <satnarinecj101@gmail.com>
2024-11-06 18:22:04 -0500

    Pixel stuff works

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
-#include <iostream>
+#include "tgaimage.h"
 
-using namespace std;
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
 
-int main() {
-    cout << "Hello world!\n";
+int main(int argc, char** argv) {
+    TGAImage image(100, 100, TGAImage::RGB);
+    image.set(50, 50, red);
+    //image.flipVertically();
+    image.write_tga_file("output.tga");
     return 0;
 }
