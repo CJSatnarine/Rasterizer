4a093cfadfa7a503de7787e7eeb2452142db31dd
CJSatnarine <satnarinecj101@gmail.com>
2024-11-06 23:18:02 -0500

    Lines!!! Lines are Drawn!!!

--- main.cpp
+++ main.cpp
@@ -1,12 +1,55 @@
 #include "tgaimage.h"
+using namespace std;
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
+const TGAColor blue = TGAColor(0, 0, 255, 255);
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor colour);
 
 int main(int argc, char** argv) {
     TGAImage image(100, 100, TGAImage::RGB);
-    image.set(50, 50, red);
-    //image.flipVertically();
+    line(13, 20, 80, 40, image, white); 
+    line(20, 13, 40, 80, image, red); 
+    line(78, 56, 100, 100, image, blue);
+    line(0, 0, 100, 100, image, green);
+    image.flip_vertically();
     image.write_tga_file("output.tga");
+
     return 0;
+}
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor colour) {
+    bool steep = false;
+    if (abs(x0 - x1) < abs(y0 - y1)) {
+        swap(x0, y0);
+        swap(x1, y1);
+        steep = true;
+    }
+
+    if (x0 > x1) { // Make it left to right. 
+        swap(x0, x1);
+        swap(y0, y1);
+    }
+
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int dError2 = abs(dy) * 2;
+    int error2 = 0;
+    int y = y0;
+
+    for (int x = x0; x <= x1; x++) {
+        if (steep) {
+            image.set(y, x, colour);
+        } 
+        else {
+            image.set(x, y, colour);
+        }
+        error2 += dError2;
+        if (error2 > dx) {
+            y += (y1 > y0 ? 1 : -1);
+            error2 -= dx * 2;
+        }
+    }
 }
